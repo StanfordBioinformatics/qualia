c759b615f2475697ba4db4716634a14b050fdd93
Greg <gregorymcinnes@gmail.com>
2015-08-19 07:29:00 -0700

    fastqc updates

--- bin/fastqc_analysis.R
+++ bin/fastqc_analysis.R
@@ -47,36 +47,49 @@ if(is.null(argsL$path)) {
 drive_id = argsL$drive_id
 fastqc_output = as.character(argsL$path)
 
+
+#### TESTING
+#fastqc_output = '~/projects/mvp/claritas/fastqc/'
+#drive_id = 'CLA00127'
+####
+
 print("Running analysis of FastQC results")
 print(paste("Drive ID: ", drive_id, sep=""))
 print(paste("FastQC Directory: ", fastqc_output, sep=""))
 
 ## Start script
 require(ggplot2)
-setwd("~/src/mvp_exome_qc/bin")
+setwd("~/src/qualia/bin")
 source("./fastqc_helper.R")
 
 # Build reference
-reference_filename = '~/src/mvp_exome_qc/data/IonTorrent_NA12878_V3.fastqc_data.txt'
+reference_filename = '~/src/qualia/data/IonTorrent_NA12878_V3.fastqc_data.txt'
 reference_lines = readLines(reference_filename)
+# Define dataframes to store results
 for (i in 1:length(reference_lines)) {
   if (grepl(">>Per base sequence quality", reference_lines[i])) {
     reference_mean_base_quality <- MeanPerBaseQuality(reference_lines, i+2)
     print(paste("Reference Mean Base Quality: ", reference_mean_base_quality))
     reference_base_quality <- ExtractPerBaseQuality(reference_lines, i+2)
+    reference_base_quality$SampleName = 'NA12878'
   } else if (grepl(">>Per sequence quality scores", reference_lines[i])) {
     reference_mean_sequence_quality <- MeanQuality(reference_lines, i+2)
     print(paste("Reference Mean Sequence Quality: ", reference_mean_sequence_quality))
   } else if (grepl(">>Per sequence GC content", reference_lines[i])) {
     reference_mean_gc_content <- MeanQuality(reference_lines, i+2)
     print(paste("Reference Mean GC Content: ", reference_mean_gc_content))
+  } else if (grepl(">>Sequence Length Distribution", reference_lines[i])) {
+    reference_median_sequence_length <- MedianSequenceLength(reference_lines, i+2)
+    print(paste("Reference Median Sequence Length: ", reference_median_sequence_length))
   }
 }
 
 # Read in all other files
 sample_files <- list.files(fastqc_output)
 sample_df <- data.frame("SampleName" = character(0), "MeanBaseQuality" = numeric(0), "MeanSequenceQuality" = numeric(0),
-                        "MeanGCContent" = numeric(0), "LowPerBaseQuality" = logical(0), "HighKmerContent" = logical(0))
+                        "MeanGCContent" = numeric(0), "LowPerBaseQuality" = logical(0), "HighKmerContent" = logical(0),
+                        "MedianSequenceLength" = numeric(0))
+per_base_quality_df <- data.frame("SampleName" = character(0), "Range" = character(0), "Mean" = numeric(0), "Median" = numeric(0))
 for (i in 1:length(sample_files)) {
   current_file <- sample_files[i]
   print(current_file)
@@ -87,20 +100,29 @@ for (i in 1:length(sample_files)) {
       mean_base_quality <- MeanPerBaseQuality(file_content, i+2)
       per_base_quality <- ExtractPerBaseQuality(file_content, i+2)
       low_per_base_quality <- LowPerBaseQuality(reference_base_quality, per_base_quality)
+      per_base_quality$SampleName <- sample_name
+      per_base_quality_df <- rbind(per_base_quality_df, per_base_quality)
     } else if (grepl(">>Per sequence quality scores", file_content[i])) {
       mean_sequence_quality <- MeanQuality(file_content, i+2)
     } else if (grepl(">>Per sequence GC content", file_content[i])) {
       mean_gc_content <- MeanQuality(file_content, i+2)
     } else if (grepl(">>Kmer Content", file_content[i])) {
       kmer_content <- CheckKmerContent(file_content, i+2)
+    } else if (grepl(">>Sequence Length Distribution", file_content[i])) {
+      median_sequence_length <- MedianSequenceLength(file_content, i+2)
     }
   }
   row = data.frame("SampleName" = sample_name, "MeanBaseQuality" = mean_base_quality, 
                    "MeanSequenceQuality" = mean_sequence_quality, "LowPerBaseQuality" = low_per_base_quality, 
-                   "MeanGCContent" = mean_gc_content, "HighKmerContent" = kmer_content)
+                   "MeanGCContent" = mean_gc_content, "HighKmerContent" = kmer_content, "MedianSequenceLength" = median_sequence_length)
   sample_df <- rbind(sample_df, row)
 }
 
+# Write raw data
+write.csv(per_base_quality_df, paste(drive_id, "base-quality.csv", sep="."))
+write.csv(sample_df, paste(drive_id, "fastqc-results", sep="."))
+
+# Create plots
 plot_theme = theme_minimal(base_size = 24, base_family = "Helvetica") + 
   theme(axis.line = element_line(colour = "black"),
         panel.grid = element_blank())
@@ -140,3 +162,24 @@ ggplot(sample_df) +
         axis.ticks.x = element_blank())
 
 ggsave(paste(drive_id, "gc-content.png", sep="."))
+
+ggplot(sample_df) +
+  geom_point(aes(SampleName, MedianSequenceLength), size=4) +
+  geom_hline(yintercept = reference_median_sequence_length) +
+  ggtitle("Median sequence length") +
+  xlab("Sample") +
+  ylab("Median sequence length") +
+  plot_theme +
+  theme(axis.text.x = element_blank(),
+        axis.ticks.x = element_blank())
+
+ggsave(paste(drive_id, "sequence-length.png", sep="."))
+
+ggplot(per_base_quality_df) +
+  geom_point(aes(Range, Mean, color=SampleName), alpha = 0.4, size = 3) +
+  geom_point(data = reference_base_quality, aes(Range, Mean), size = 4) +
+  plot_theme + 
+  theme(axis.text.x = element_text(angle = 90, hjust = 1),
+        legend.position="none")
+
+ggsave(paste(drive_id, "base-quality-all.png", sep="."))
--- bin/fastqc_helper.R
+++ bin/fastqc_helper.R
@@ -1,4 +1,5 @@
 require(plyr)
+require(cwhmisc)
 
 MeanPerBaseQuality <- function(lines, i) {
   sum <- 0
@@ -36,6 +37,17 @@ ExtractPerBaseQuality <- function(lines, i) {
   df <- data.frame("Range" = character(0), "Mean" = numeric(0), "Median" = numeric(0))
   while (!grepl(">>END_MODULE", lines[i])) {
     current <- strsplit(lines[i],"\t")[[1]]
+    if (nchar(current[1]) > 1) {
+      values <- strsplit(current[1], "-")[[1]]
+      if (length(values) > 1 && as.numeric(values[2]) %% 10 != 9) {
+        values[2] <- round_any(as.numeric(values[2]), 10, f=ceiling) - 1
+        current[1] = paste(values[1], as.character(values[2]), sep="-")
+      }
+      else if (length(values == 1)) {
+        values[2] <- as.numeric(values[1]) + 9
+        current[1] = paste(values[1], as.character(values[2]), sep="-")
+      }
+    }
     new_row = data.frame("Range" = current[1], "Mean" = as.numeric(current[2]), "Median" = as.numeric(current[3]))
     df = rbind(df, new_row)
     i <- i + 1
@@ -46,10 +58,10 @@ ExtractPerBaseQuality <- function(lines, i) {
 CheckKmerContent <- function(lines, i) {
   df <- data.frame("Sequence" = character(0), "PValue" = numeric(0), "Position" = character(0))
   while (!grepl(">>END_MODULE", lines[i])) {
-    current <- strsplit(lines[i],"\t")[[1]]
+    current <- strsplit(lines[i], "\t")[[1]]
     range = strsplit(current[5], "-")[[1]]
     if (as.numeric(range[1]) <= 250) {
-      new_row = data.frame("Sequence" = current[0], "PValue" = as.numeric(current[3]), "Position" = current[5])
+      new_row = data.frame("Sequence" = current[1], "PValue" = as.numeric(current[3]), "Position" = current[5])
       df = rbind(df, new_row)
     }
     i <- i + 1
@@ -63,11 +75,23 @@ CheckKmerContent <- function(lines, i) {
 LowPerBaseQuality <- function(reference, sample) {
   merged = merge(reference, sample, all.x=TRUE, by="Range")
   merged = na.omit(merged)
-  low_quality <- merged[(merged$Mean.y < merged$Mean.x-5 && merged$Mean.x != 0) | 
-                          (merged$Median.y < merged$Median.x-5 && merged$Median.y != 0),]
+  low_quality <- merged[(merged$Mean.y < merged$Mean.x-5) | 
+                          (merged$Median.y < merged$Median.x-5 & merged$Median.y != 0),]
   if (nrow(low_quality) > 0) {
     print(low_quality)
     return(TRUE)
   }
   return(FALSE)
+}
+
+MedianSequenceLength <- function(lines, i) {
+  df <- data.frame("Range" = numeric(0), "Count" = numeric(0))
+  while (!grepl(">>END_MODULE", lines[i])) {
+    current <- strsplit(lines[i],"\t")[[1]]
+    new_row <- data.frame("Range" = as.numeric(strsplit(current[1], "-")[[1]][1]), "Count" = as.numeric(current[2]))
+    df <- rbind(df, new_row)
+    i <- i + 1
+  }
+  m <- w.median(df$Range, df$Count)
+  return(m)
 }
